5f814f48fbe79bfc755f079c159e8785508bcf6a
imujo <ivo.mujo.3@gmail.com>
2023-01-12 07:26:45 +0100

    adding table routes

--- server.js
+++ server.js
@@ -1,23 +1,25 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const db = require("./config/database");
 const cors = require("cors");
 require("dotenv").config();
 const eventRoutes = require("./routes/eventRoutes");
 const roomRoutes = require("./routes/roomRoutes");
-
-app.use(cors());
-app.use(bodyParser.json());
+const tableRoutes = require("./routes/tableRoutes");
 
 const errorHandler = (error, request, response, next) => {
   console.error(`ERROR - ${error.message}`);
   const status = error.status || 400;
   response.status(status).json({ msg: error.message });
+  next();
 };
+
+app.use(cors());
+app.use(bodyParser.json());
 
 app.use(eventRoutes);
 app.use(roomRoutes);
+app.use(tableRoutes);
 
 app.use(errorHandler);
 
